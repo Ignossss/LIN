3897e9f1eb573e77c2b960b7dbe47bc5d270c9cb
Ignossss <punkypyke@gmail.com>
2025-11-17 06:04:35 +0000

    Update all.sh

--- all.sh
+++ all.sh
@@ -6,14 +6,10 @@ pacman -Syu
 pacman -S mesa vulkan-radeon nvidia nvidia-utils nvidia-settings vulkan-icd-loader
 pacman -S pipewire-alsa pipewire-jack pipewire-pulse pamixer pavucontrol
 pacman -S ly mako bluez bluez-utils grim slurp
-pacman -S kitty thunar dmenu wofi waybar 
-pacman -S obsidian prismlauncher steam code
-pacman -S tk
+pacman -S kitty thunar wofi
+pacman -S obsidian
 
 pacman -S hyprland hyprpaper xdg-desktop-portal xdg-desktop-portal-hyprland 
-
-pacman -S xorg bspwm sxhkd picom
-xauth generate :0 . trusted
 
 pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-font-awesome ttf-noto-nerd 
 
@@ -24,6 +20,3 @@ systemctl enable bluetooth
 pacman -S os-prober
 
 echo Enable os-prober: helix /etc/default/grub \|\| grub-mkconfig -o /boot/grub/grub.cfg
-echo Place the dotfiles: cp \(LIN/.config/\)FOLDER ~/.config/FOLDER/
-echo Chmod \"bspwm\" \in ~/.config/bspwm/ \|\| chmod +x bspwm
-echo Do: 
