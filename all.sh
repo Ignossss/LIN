461c785f8675c8dbb1cb141a8e5142093225d54b
Ignossss <punkypyke@gmail.com>
2025-11-22 02:17:55 -0800

    Add waybar and code to package installation

--- all.sh
+++ all.sh
@@ -6,10 +6,13 @@ pacman -Syu
 pacman -S mesa vulkan-radeon nvidia nvidia-utils nvidia-settings vulkan-icd-loader
 pacman -S pipewire-alsa pipewire-jack pipewire-pulse pamixer pavucontrol
 pacman -S ly mako bluez bluez-utils grim slurp
-pacman -S kitty thunar wofi
-pacman -S obsidian nwg-look
+pacman -S kitty thunar wofi waybar
+pacman -S obsidian nwg-look code tk
 
 pacman -S hyprland hyprpaper xdg-desktop-portal xdg-desktop-portal-hyprland 
+
+pacman -S xorg bspwm sxhkd picom
+xauth generate :0 . trusted
 
 pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-font-awesome ttf-noto-nerd 
 
